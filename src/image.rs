92098f6f22a015dbb66ddfb2b6f9dfe67e692947
lunabunn <iamrabbitmoon@gmail.com>
2022-06-04 02:58:49 -0400

    Add image update functions

--- src/image.rs
+++ src/image.rs
@@ -6,6 +6,10 @@ use crate::Context;
 
 pub struct Image {
     pub(crate) inner: glow::Texture,
+    width: u32,
+    height: u32,
+    format: u32,
+    kind: u32,
     ctx: Rc<glow::Context>,
 }
 
@@ -36,25 +40,25 @@ impl Image {
         filter: ImageFilter,
         wrap: ImageWrap,
     ) -> Self {
+        let format = match pixel_format {
+            ImageFormat::Rgb8 => glow::RGB,
+            ImageFormat::Rgba8 => glow::RGBA,
+        };
+        let kind = match pixel_format {
+            ImageFormat::Rgb8 | ImageFormat::Rgba8 => glow::UNSIGNED_BYTE,
+        };
+        let filter = match filter {
+            ImageFilter::Nearest => glow::NEAREST,
+            ImageFilter::Linear => glow::LINEAR,
+        };
+        let wrap = match wrap {
+            ImageWrap::Clamp => glow::CLAMP_TO_EDGE,
+            ImageWrap::Repeat => glow::REPEAT,
+        };
+
         let inner = unsafe {
             let texture = ctx.inner.create_texture().unwrap();
             ctx.inner.bind_texture(glow::TEXTURE_2D, Some(texture)); // TODO: other texture types
-
-            let format = match pixel_format {
-                ImageFormat::Rgb8 => glow::RGB,
-                ImageFormat::Rgba8 => glow::RGBA,
-            };
-            let kind = match pixel_format {
-                ImageFormat::Rgb8 | ImageFormat::Rgba8 => glow::UNSIGNED_BYTE,
-            };
-            let filter = match filter {
-                ImageFilter::Nearest => glow::NEAREST,
-                ImageFilter::Linear => glow::LINEAR,
-            };
-            let wrap = match wrap {
-                ImageWrap::Clamp => glow::CLAMP_TO_EDGE,
-                ImageWrap::Repeat => glow::REPEAT,
-            };
 
             ctx.inner.tex_image_2d(
                 glow::TEXTURE_2D,
@@ -83,6 +87,10 @@ impl Image {
 
         Self {
             inner,
+            width,
+            height,
+            format,
+            kind,
             ctx: ctx.inner.clone(),
         }
     }
@@ -96,25 +104,25 @@ impl Image {
         wrap: ImageWrap,
         data: &[u8],
     ) -> Self {
+        let format = match pixel_format {
+            ImageFormat::Rgb8 => glow::RGB,
+            ImageFormat::Rgba8 => glow::RGBA,
+        };
+        let kind = match pixel_format {
+            ImageFormat::Rgb8 | ImageFormat::Rgba8 => glow::UNSIGNED_BYTE,
+        };
+        let filter = match filter {
+            ImageFilter::Nearest => glow::NEAREST,
+            ImageFilter::Linear => glow::LINEAR,
+        };
+        let wrap = match wrap {
+            ImageWrap::Clamp => glow::CLAMP_TO_EDGE,
+            ImageWrap::Repeat => glow::REPEAT,
+        };
+
         let inner = unsafe {
             let texture = ctx.inner.create_texture().unwrap();
             ctx.inner.bind_texture(glow::TEXTURE_2D, Some(texture)); // TODO: other texture types
-
-            let format = match pixel_format {
-                ImageFormat::Rgb8 => glow::RGB,
-                ImageFormat::Rgba8 => glow::RGBA,
-            };
-            let kind = match pixel_format {
-                ImageFormat::Rgb8 | ImageFormat::Rgba8 => glow::UNSIGNED_BYTE,
-            };
-            let filter = match filter {
-                ImageFilter::Nearest => glow::NEAREST,
-                ImageFilter::Linear => glow::LINEAR,
-            };
-            let wrap = match wrap {
-                ImageWrap::Clamp => glow::CLAMP_TO_EDGE,
-                ImageWrap::Repeat => glow::REPEAT,
-            };
 
             ctx.inner.tex_image_2d(
                 glow::TEXTURE_2D,
@@ -143,7 +151,32 @@ impl Image {
 
         Self {
             inner,
+            width,
+            height,
+            format,
+            kind,
             ctx: ctx.inner.clone(),
+        }
+    }
+
+    pub fn update(&self, data: &[u8]) {
+        self.update_part(0, 0, self.width, self.height, data);
+    }
+
+    pub fn update_part(&self, x: u32, y: u32, width: u32, height: u32, data: &[u8]) {
+        unsafe {
+            self.ctx.bind_texture(glow::TEXTURE_2D, Some(self.inner));
+            self.ctx.tex_sub_image_2d(
+                glow::TEXTURE_2D,
+                0,
+                x as _,
+                y as _,
+                width as _,
+                height as _,
+                self.format,
+                self.kind,
+                glow::PixelUnpackData::Slice(&data),
+            );
         }
     }
 }
